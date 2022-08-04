26aa297dfb1001efebadebccd16588953336a794
Seongwoo Shin <asdfgg77@naver.com>
2022-08-05 02:58:43 +0900

    reload 구현(무한로딩 제거)

--- index.js
+++ index.js
@@ -2,31 +2,17 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const btn = document.querySelector("button");
 
-var raf;
-
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = "black";
-
 //막대기
 var bar = {
   x: 150,
   y: 350,
   vx: 15,
   width: 100,
+  height: 20,
   color: "black",
-  draw: function () {
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "black";
-    ctx.beginPath();
-    ctx.fillRect(this.x, this.y, 100, 20);
-    ctx.closePath();
-    if (this.x + 100 > canvas.width) {
-      this.x = canvas.width - 100;
-    } else if (this.x < 0) {
-      this.x = 0;
-    }
-  },
 };
+
+//벽돌
 var brick = {
   rowCount: 3,
   columnCount: 4,
@@ -35,43 +21,8 @@ var brick = {
   padding: 10,
   offSetTop: 30,
   offSetLeft: 30,
+  color: "green",
 };
-
-var bricks = [];
-
-for (var i = 0; i < brick.columnCount; i++) {
-  bricks[i] = [];
-  for (var j = 0; j < brick.rowCount; j++) {
-    bricks[i][j] = { x: 0, y: 0, status: 1 };
-  }
-}
-
-function rightMove() {
-  bar.x += bar.vx;
-  ctx.clearRect(bar.x - bar.vx, 350, 100, 20);
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = "black";
-  bar.draw();
-}
-function leftMove() {
-  bar.x -= bar.vx;
-  ctx.clearRect(bar.x + bar.vx, 350, 100, 20);
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = "black";
-  bar.draw();
-}
-
-window.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowRight") {
-    raf = window.requestAnimationFrame(rightMove);
-  }
-});
-window.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowLeft") {
-    raf = window.requestAnimationFrame(leftMove);
-  }
-});
-bar.draw();
 
 // 공
 var ball = {
@@ -81,86 +32,72 @@ var ball = {
   vy: 2,
   radius: 15,
   color: "black",
-  draw: function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    bar.draw();
-    drawBricks();
-    collisionDetection();
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    if (
-      this.x + this.vx > canvas.width - this.radius ||
-      this.x + this.vx < this.radius
-    ) {
-      this.vx = -this.vx;
-    }
-    if (this.y + this.vy < this.radius) {
-      this.vy = -this.vy;
-    } else if (ball.y + ball.vy > canvas.height - ball.radius) {
-      if (ball.x > bar.x && ball.x < bar.x + 100) {
-        ball.vy = -ball.vy;
-      } else {
-        alert("Game Over");
-        document.location.reload();
-      }
-    }
-  },
 };
 
-function draw() {
-  if (
-    ball.y + ball.radius === bar.y &&
-    ball.x >= bar.x &&
-    ball.x <= bar.x + bar.width
-  ) {
-    ctx.beginPath();
-    ctx.clearRect(
-      ball.x - ball.radius - ball.vx,
-      ball.y - ball.radius - ball.vy,
-      ball.radius * 2,
-      ball.radius * 2
-    );
-    ctx.closePath();
-    ball.vy = -ball.vy;
-    ball.vx = -ball.vx;
-    ball.x += ball.vx;
-    ball.y += ball.vy;
-    ball.draw();
-    ctx.beginPath();
-    ctx.clearRect(
-      ball.x - ball.radius - ball.vx,
-      ball.y - ball.radius - ball.vy,
-      ball.radius * 2,
-      ball.radius * 2
-    );
-    ctx.closePath();
-    raf = window.requestAnimationFrame(draw);
-  } else {
-    ball.x += ball.vx;
-    ball.y += ball.vy;
-    ctx.beginPath();
-    ctx.clearRect(
-      ball.x - ball.radius - ball.vx,
-      ball.y - ball.radius - ball.vy,
-      ball.radius * 2,
-      ball.radius * 2
-    );
-    ctx.closePath();
-    ball.draw();
-    raf = window.requestAnimationFrame(draw);
-  }
+//이벤트
+function rightMove() {
+  bar.x += bar.vx;
+}
+function leftMove() {
+  bar.x -= bar.vx;
+}
 
-  if (ball.y === canvas.height) {
-    window.cancelAnimationFrame(draw);
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight") {
+    window.requestAnimationFrame(rightMove);
+  }
+});
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft") {
+    window.requestAnimationFrame(leftMove);
+  }
+});
+btn.addEventListener("click", function (e) {
+  window.requestAnimationFrame(draw);
+});
+
+//함수
+function draw() {
+  drawBall();
+  drawBar();
+  drawBricks();
+  collisionDetection();
+}
+
+function drawBall() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fillStyle = ball.color;
+  ctx.fill();
+}
+
+function drawBar() {
+  ctx.beginPath();
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = bar.color;
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle = bar.color;
+  ctx.fillRect(bar.x, bar.y, bar.width, bar.height);
+  ctx.closePath();
+
+  if (bar.x + 100 > canvas.width) {
+    bar.x = canvas.width - 100;
+  } else if (bar.x < 0) {
+    bar.x = 0;
   }
 }
 
-btn.addEventListener("click", function (e) {
-  raf = window.requestAnimationFrame(draw);
-});
+var bricks = [];
+for (var i = 0; i < brick.columnCount; i++) {
+  bricks[i] = [];
+  for (var j = 0; j < brick.rowCount; j++) {
+    bricks[i][j] = { x: 0, y: 0, status: 1 };
+  }
+}
 
 function drawBricks() {
   for (var i = 0; i < brick.columnCount; i++) {
@@ -179,7 +116,59 @@ function drawBricks() {
     }
   }
 }
+
 function collisionDetection() {
+  if (
+    ball.y + ball.vy + ball.radius > bar.y &&
+    ball.y + ball.vy + ball.radius < bar.y + bar.height &&
+    ball.x >= bar.x &&
+    ball.x <= bar.x + bar.width
+  ) {
+    ctx.beginPath();
+    ctx.arc(
+      ball.x - ball.radius - ball.vx,
+      ball.y - ball.radius - ball.vy,
+      ball.radius,
+      0,
+      true
+    );
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ball.vy = -ball.vy;
+    ball.vx = -ball.vx;
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    window.requestAnimationFrame(draw);
+  } else {
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    window.requestAnimationFrame(draw);
+  }
+
+  if (
+    ball.x + ball.vx > canvas.width - ball.radius ||
+    ball.x + ball.vx < ball.radius
+  ) {
+    ball.vx = -ball.vx;
+  }
+
+  //천장 충돌
+  if (ball.y + ball.vy < ball.radius) {
+    ball.vy = -ball.vy;
+  }
+
+  //바닥 충돌
+  if (ball.y + ball.vy > canvas.height) {
+    ball.vx = 0;
+    ball.vy = 0;
+    const userName = prompt("이름?", "홍길동");
+    location.reload(true);
+  }
+
   for (var i = 0; i < brick.columnCount; i++) {
     for (var j = 0; j < brick.rowCount; j++) {
       var b = bricks[i][j];
@@ -198,4 +187,8 @@ function collisionDetection() {
   }
 }
 
-ball.draw();
+function moveBall() {}
+
+drawBall();
+drawBar();
+drawBricks();
